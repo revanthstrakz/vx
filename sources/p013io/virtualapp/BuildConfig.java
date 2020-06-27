7e8cef0f0da9830246d3362341725b8e3c25e0fa
Revanth <dr.revanthstrakz@gmail.com>
2020-06-28 02:09:20 +0530

    changed

--- sources/p013io/virtualapp/BuildConfig.java
+++ sources/p013io/virtualapp/BuildConfig.java
@@ -4,7 +4,7 @@ package p013io.virtualapp;
 public final class BuildConfig {
     public static final String APPLICATION_ID = "io.va.exposed";
     public static final String BUILD_TYPE = "release";
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
     public static final String FLAVOR = "aosp";
     public static final int VERSION_CODE = 182;
     public static final String VERSION_NAME = "0.18.2";
